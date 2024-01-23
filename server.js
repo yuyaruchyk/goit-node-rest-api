00b1907530c2ca4c8bef071cb333596c726cbfce
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2024-01-23 13:30:07 +0100

    add port

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log("Database connection successful");
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
-    });
+    app.listen(PORT);
   })
+
   .catch((error) => {
-    console.error("Error connecting to MongoDB:", error.message);
+    console.log(error.message);
     process.exit(1);
   });
