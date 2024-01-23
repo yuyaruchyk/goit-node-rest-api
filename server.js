a778c7c88d7dfc5fe8a1be87f6fbff76b3a8da24
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2024-01-23 12:57:25 +0100

    add

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const { DB_HOST } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("stringQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  });
