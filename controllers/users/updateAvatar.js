193388e2d7ee1bbc7aaf03eeefd4e952f2f18943
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2024-02-08 22:08:39 +0100

    add jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs/promises");
 const { ctrlWrapper } = require("../../helpers/ctrlWrapper");
-
+const Jimp = require("jimp");
 const { User } = require("../../models/user");
 
 const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
@@ -10,12 +10,18 @@ const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
 
-  res.json({
+  const image = await Jimp.read(tempUpload);
+  image.resize(250, 250);
+
+  const resultUpload = path.join(avatarsDir, filename);
+  await image.writeAsync(resultUpload);
+  await fs.unlink(tempUpload);
+
+  const avatarURL = path.join("avatars", filename);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.status(200).json({
     avatarURL,
   });
 };
