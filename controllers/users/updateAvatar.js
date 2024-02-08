dc609a67fbd61e4c63bec0229437a781744a6af4
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2024-02-08 10:15:59 +0100

    update uplode avatar

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,8 +1,7 @@
 const path = require("path");
 const fs = require("fs/promises");
-const Jimp = require("jimp");
-const { User } = require("../../models/user");
 
+const { User } = require("../../models/user");
 const { ctrlWrapper } = require("../../helpers");
 
 const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
@@ -11,14 +10,8 @@ const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
-
-  const image = await Jimp.read(tempUpload);
-  image.resize(250, 250);
-
   const resultUpload = path.join(avatarsDir, filename);
-  await image.writeAsync(resultUpload);
-  await fs.unlink(tempUpload);
-
+  await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
